dd771e8b586c84c35b00320384308984c8e0e357
Dan Bender <hello@danbender.net>
2015-08-16 16:51:24 +0200

    create new TaskModel; added ObjectiveC bridge

---
+++ TaskModel.swift
@@ -0,0 +1,12 @@
+import Foundation
+import CoreData
+
+@objc(TaskModel)
+class TaskModel: NSManagedObject {
+
+    @NSManaged var completed: NSNumber
+    @NSManaged var date: NSDate
+    @NSManaged var subtask: String
+    @NSManaged var task: String
+
+}
