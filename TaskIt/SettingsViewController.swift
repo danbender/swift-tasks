a9bf895962f4ec9ac7878fd3590dd9e329607c20
Dan Bender <hello@danbender.net>
2015-09-06 01:50:56 +0400

    added SettingsViewController with 2 tables + table setup

---
+++ TaskIt/SettingsViewController.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var capitalizeTableView: UITableView!
+    @IBOutlet weak var completeNewTodoTableView: UITableView!
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    let kVersionNumber = "1.0"
+    
+    let kShouldCapitalizeTaskKey = "shouldCapitalizeTask"
+    let kShouldCompleteNewTodoKey = "completeNewTodo"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.capitalizeTableView.delegate = self
+        self.capitalizeTableView.dataSource = self
+        self.capitalizeTableView.scrollEnabled = false
+        
+        self.completeNewTodoTableView.delegate = self
+        self.completeNewTodoTableView.dataSource = self
+        self.completeNewTodoTableView.scrollEnabled = false
+        
+        self.title = "Settings"
+        self.versionLabel.text = kVersionNumber
+        
+//        overwrite default back button
+        var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("doneBarButtonItemPressed:"))
+        self.navigationItem.leftBarButtonItem = doneButton
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func doneBarButtonItemPressed (barButtonItem: UIBarButtonItem) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if tableView == self.capitalizeTableView {
+            var capitalizeCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("capitalizeCell") as! UITableViewCell
+            
+            if indexPath.row == 0 {
+                capitalizeCell.textLabel!.text = "No,do not Capitalize"
+                if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCapitalizeTaskKey) == false {
+                    capitalizeCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                } else {
+                    capitalizeCell.accessoryType = UITableViewCellAccessoryType.None
+                }
+            }
+            else {
+                capitalizeCell.textLabel!.text = "Yes, Capitalize!"
+                
+                if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCapitalizeTaskKey) == true {
+                    capitalizeCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                } else {
+                    capitalizeCell.accessoryType = UITableViewCellAccessoryType.None
+                }
+            }
+            return capitalizeCell
+        }
+        else {
+            var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("completeNewTodoCell") as! UITableViewCell
+            if indexPath.row == 0 {
+                cell.textLabel!.text = "Do not complete Task"
+                if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCompleteNewTodoKey) == false {
+                    cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                }
+                else {
+                    cell.accessoryType = UITableViewCellAccessoryType.None
+                }
+            }
+            else {
+                cell.textLabel!.text = "Complete Task"
+                if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCompleteNewTodoKey) == true{
+                    cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                }
+                else {
+                    cell.accessoryType = UITableViewCellAccessoryType.None
+                }
+            }
+            return cell
+        }
+    }
+
+    
+    
+    
+}
