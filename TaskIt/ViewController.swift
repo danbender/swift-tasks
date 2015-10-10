c12a4c02a733a0e0a2d1a685a0485aa225248ccf
Dan Bender <hello@danbender.net>
2015-10-10 16:39:42 +0200

    Updating the taskDetailEdited function; show UIAlert

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -152,7 +152,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 //    TaskDetailViewControllerDelegate
     
     func taskDetailEdited() {
-        println("taskDetailEdited()")
+        showAlert()
+    }
+    
+    func showAlert () {
+        var alert = UIAlertController(title: "Change Made!", message: "Congratulations", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok!", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
 }
